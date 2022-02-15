c1a9a32128d9526b1a47fc78f308976fe734657c
Fahmi Fauzan <muhamadfahmi.f@gmail.com>
2022-02-15 15:30:39 +0700

    layout loginpage

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -12,9 +12,9 @@ function signin() {
         <>
             <div className='md:flex md:flex-row'>
                 <div className='mt-32 mx-16 md:shrink-0 md:pl-32'>
-                    <Image src={LogoLogin} alt='logo login' width={300} height={300} ></Image>
+                    <Image src={LogoLogin} alt='logo signin' width={300} height={300} ></Image>
                 </div>
-                <div className='continer max-w-sm h-auto mx-auto mt-32 content-end bg-white drop-shadow-xl rounded-xl md:'>
+                <div className='continer max-w-sm h-auto mx-auto mt-32 content-end bg-white drop-shadow-xl rounded-xl'>
                     <h2 className='text-center pt-5 text-2xl text-primary'>Sign In</h2>
                     <form action="">
                         <div className='mt-5 mx-12'>
