074edf10018b642b6707a3ae7cca4ffa87b22504
akmalays <akmalays@gmail.com>
2022-02-16 21:45:52 +0700

    add navbar and footer component

---
+++ components/navbar.jsx
@@ -0,0 +1,192 @@
+import { useState } from "react";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import imgLogo1 from "../img/st,small,507x507-pad,600x600,f8f8f8 1.png";
+import imgLogo2 from "../img/Group 107.png";
+import Image from "next/image";
+
+function navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  return (
+    <div>
+      <div className="bg-white drop-shadow-lg">
+        <div className="flex flex-col items-center lg:flex-row">
+          <div className=" flex pt-3 px-5">
+            <div className="w-30">
+              <Image src={imgLogo1} />
+            </div>
+            <Image src={imgLogo2} />
+          </div>
+          <div className="flex items-center justify-between px-4 py-3 lg:py-0 border-b-2 lg:border-b-0">
+            <div>
+              <button
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+                className=" focus:outline-none text-black block lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    className={!isOpen ? "block" : "hidden"}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                  <path
+                    className={isOpen ? "block" : "hidden"}
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } lg:flex flex-col lg:flex-row justify-between w-full py-4 lg:py-0`}
+          >
+            <div className="flex flex-col lg:flex-row">
+              <a
+                href="#"
+                className="block px-4 py-2 lg:py-5 font-semibold text-black hover:text-rose-600"
+              >
+                Home
+              </a>
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="inline-flex w-full font-semibold px-4 py-2 lg:py-5 hover:text-rose-600">
+                    Categories
+                    <ChevronDownIcon
+                      className="-mr-1 ml-2 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Action Figures
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Trading Card
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Model Kit Selection
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <form method="POST" action="#">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              type="submit"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "block w-full text-left px-4 py-2 text-sm"
+                              )}
+                            >
+                              Accessories and Other Stuff
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </form>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+              <a
+                href="#"
+                className="block px-4 py-2 lg:py-5 font-semibold text-black hover:text-rose-600"
+              >
+                {" "}
+                Help
+              </a>
+            </div>
+
+            <div className="flex flex-col lg:flex-row">
+              <a
+                href="#"
+                className="block px-4 py-2 font-semibold lg:py-5 lg:px-10 text-black  hover:text-rose-600"
+              >
+                {" "}
+                Create Account
+              </a>
+              <div className="block px-4 py-2 lg:py-5 space-x-1">
+                <a
+                  href=""
+                  className=" px-8 py-3 bg-rose-600 font-semibold  hover:bg-white text-white hover:text-yellow-800 rounded-2xl transition duration-300"
+                >
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
