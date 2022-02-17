57b2fe8c0c1f5de5a326acb4871755a9f6701648
Fahmi Fauzan <muhamadfahmi.f@gmail.com>
2022-02-17 11:15:42 +0700

    repush homepage

---
+++ components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+function Layout({ children }) {
+
+    return (
+        <>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
