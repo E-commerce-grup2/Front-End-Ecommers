91bac4763b590aa8e18265688012c4001f2e680f
akmalays <akmalays@gmail.com>
2022-02-20 14:15:15 +0700

    add route on categories

---
+++ pages/adminPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function adminPage() {
+  return (
+    <div>
+      <div class="mt-10 mx-20 mb-10 grid gap-14 md:grid-cols-1 items-start">
+        <div class="container shadow-lg rounded-xl px-8 pt-3 bg-gray-200"></div>
+      </div>
+    </div>
+  );
+}
+
+export default adminPage;
